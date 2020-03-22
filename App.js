0bcdb754ff9e064482b30ab2861cfa235a7f36a1
Laura Armfield-Perez <lca13@pitt.edu>
2020-03-22 16:38:19 -0400

    feat: add tab bar icons

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {View, Text} from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'
+
 import ImagePick from './ImagePicker';
 
 const Tab = createBottomTabNavigator();
@@ -28,9 +30,30 @@ function Profile(){
 export default function App() {
     return (
     <NavigationContainer>
-      <Tab.Navigator>
+            <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color}) => {
+            let iconName;
+
+            if (route.name === 'home') {
+              iconName = 'ios-home';
+            }
+            if (route.name === 'upload') {
+              iconName = 'md-photos';
+            }
+            if (route.name === 'profile'){
+              iconName = 'ios-happy'
+            }
+
+            return <Ionicons name={iconName} size={24} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+        }}>
         <Tab.Screen name="home" component={Home} />
-        <Tab.Screen name="pick" component={ImagePick} />
+        <Tab.Screen name="upload" component={ImagePick} />
         <Tab.Screen name="profile" component={Profile} />
       </Tab.Navigator>
     </NavigationContainer>
