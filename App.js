84fe3668fcf8ad4619ba8a29f9be02e38ef1f15f
Angela Vuong <vuong.angela@gmail.com>
2020-03-22 15:06:48 -0400

    added navigator component

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View,} from 'react-native';
-import ImagePick from './ImagePicker'
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import ImagePick from './ImagePicker';
+import Navigator from './client/component/navigator';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <ImagePick />
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <ImagePick /> */}
+        <Navigator />
       </View>
     );
   }
