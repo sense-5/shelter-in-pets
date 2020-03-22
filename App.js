3cf975518b6a46cc0e5c58faf87eb485a347b367
Laura Armfield-Perez <lca13@pitt.edu>
2020-03-22 16:21:46 -0400

    feat: tab bar structure created

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import 'react-native-gesture-handler'
 import * as React from 'react';
+
+import {View, Text} from 'react-native'
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ImagePick from './ImagePicker';
 
 const Tab = createBottomTabNavigator();
 
+function Home(){
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
+      <Text>home</Text>
+    </View>
+  )
+}
+
+function Profile(){
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: "center"}}>
+      <Text>Profile</Text>
+    </View>
+  )
+}
+
 export default function App() {
     return (
     <NavigationContainer>
       <Tab.Navigator>
         {console.log('in navigator')}
+        <Tab.Screen name="home" component={Home} />
         <Tab.Screen name="pick" component={ImagePick} />
+        <Tab.Screen name="profile" component={Profile} />
       </Tab.Navigator>
     </NavigationContainer>
 
