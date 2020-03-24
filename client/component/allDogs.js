bae8e20187ce0b50e5c34525ac2a9fed63dee2ef
Angela Vuong <vuong.angela@gmail.com>
2020-03-24 16:00:48 -0400

    added button to allDogs to request from server with localhost

--- client/component/allDogs.js
+++ client/component/allDogs.js
@@ -1,113 +1,154 @@
-import React, {Component} from 'react'
-import {View, Text, StyleSheet, ScrollView, Image} from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Button,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
+import { getAllDogs } from '../store/allDogs';
 
 const dummyData = [
-  {id: 1,
-  name: 'Rover',
-  imageUrl: 'https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg',
+  {
+    id: 1,
+    name: 'Rover',
+    imageUrl:
+      'https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg',
   },
-  {id: 2,
-  name: 'Buddy',
-  imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Rusty.jpg/1200px-Rusty.jpg',
+  {
+    id: 2,
+    name: 'Buddy',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Rusty.jpg/1200px-Rusty.jpg',
   },
-  {id: 3,
-  name: 'Zeke',
-  imageUrl: 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/02/dog-451643.jpg?h=bf654dbc&itok=MQGvBmuo',
+  {
+    id: 3,
+    name: 'Zeke',
+    imageUrl:
+      'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/02/dog-451643.jpg?h=bf654dbc&itok=MQGvBmuo',
   },
-  {id: 4,
+  {
+    id: 4,
     name: 'Mango',
-    imageUrl: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/09/04/13/istock-1031307988.jpg?w968h681',
+    imageUrl:
+      'https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/09/04/13/istock-1031307988.jpg?w968h681',
   },
-  {id: 5,
+  {
+    id: 5,
     name: 'Wiggles',
-    imageUrl: 'https://media4.s-nbcnews.com/j/newscms/2019_23/2885811/190606-border-collie-mc-1318_5b1706791f4ae9ddb3029540a98f7e08.fit-760w.JPG',
+    imageUrl:
+      'https://media4.s-nbcnews.com/j/newscms/2019_23/2885811/190606-border-collie-mc-1318_5b1706791f4ae9ddb3029540a98f7e08.fit-760w.JPG',
   },
-]
+];
 
 class AllDogs extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
+    this.handleAllDogs = this.handleAllDogs.bind(this);
   }
 
-  like(e){
-    console.log('liked')
+  handleAllDogs() {
+    this.props.getAllDogs();
+  }
+
+  like(e) {
+    console.log('liked');
     //function to add dog to user's liked dogs
     //if dog is already there remove it (un-like)
-    console.log(e)
+    console.log(e);
   }
 
-  render(){
-    const { navigation } = this.props
+  render() {
+    const { navigation } = this.props;
 
-    return(
-      <View >
-       <ScrollView>
+    return (
+      <View>
+        <ScrollView>
+          <Button title="GET DOGS" onPress={() => this.handleAllDogs()} />
           {dummyData.map(dog => {
-            return(
+            return (
               <View key={dog.id} style={styles.dogContainer}>
-
-                <TouchableWithoutFeedback onPress={()=>{
-                  navigation.navigate('Single Dog', dog)
-                }}>
-
-
+                <TouchableWithoutFeedback
+                  onPress={() => {
+                    navigation.navigate('Single Dog', dog);
+                  }}
+                >
                   <View style={styles.dogHeader}>
-                    <Image source={{uri: dog.imageUrl}} style={styles.dogIcon}/>
+                    <Image
+                      source={{ uri: dog.imageUrl }}
+                      style={styles.dogIcon}
+                    />
                     <Text style={styles.name}>{dog.name}</Text>
                   </View>
                 </TouchableWithoutFeedback>
 
-                <Image source={{uri: dog.imageUrl}} style={styles.image} />
+                <Image source={{ uri: dog.imageUrl }} style={styles.image} />
                 <View style={styles.dogFooter}>
-                  <Ionicons name={'ios-heart-empty'} size={30} onPress={this.like} />
+                  <Ionicons
+                    name={'ios-heart-empty'}
+                    size={30}
+                    onPress={this.like}
+                  />
                   {/* icon for liked dogs below */}
                   {/* <Ionicons name={'ios-heart'} size={30} onPress={this.like} color={'#de104a'}/> */}
-                  <Ionicons name={'ios-mail'} size={30} onPress={this.likeSwitch} />
+                  <Ionicons
+                    name={'ios-mail'}
+                    size={30}
+                    onPress={this.likeSwitch}
+                  />
                 </View>
-
               </View>
-            )
+            );
           })}
         </ScrollView>
       </View>
-    )
+    );
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllDogs: () => {
+      dispatch(getAllDogs());
+    },
+  };
+};
+
+const Dogs = connect(null, mapDispatchToProps)(AllDogs);
+export default Dogs;
 
 const styles = StyleSheet.create({
   dogContainer: {
     // backgroundColor: 'yellow',
-    marginBottom: 20
+    marginBottom: 20,
   },
   image: {
     height: 350,
-    width: '100%'
+    width: '100%',
   },
   name: {
     fontSize: 18,
-    padding: 5
+    padding: 5,
   },
   dogIcon: {
     width: 30,
     height: 30,
     borderRadius: 50,
-    padding: 5
+    padding: 5,
   },
   dogHeader: {
-    flexDirection:'row',
-    padding: 5
+    flexDirection: 'row',
+    padding: 5,
   },
   dogFooter: {
     flexDirection: 'row',
     // backgroundColor: 'pink',
     justifyContent: 'space-around',
     padding: 8,
-    width: '25%'
-  }
-})
-
-export default AllDogs
-
+    width: '25%',
+  },
+});
---
+++ client/store/allDogs.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+// action type
+const GET_ALL_DOGS = 'GET_ALL_DOGS';
+
+// action creator
+const gotAllDogs = allDogs => ({ type: GET_ALL_DOGS, allDogs });
+
+// inital state
+const initalState = {
+  allDogs: [],
+  selectedDog: {},
+};
+
+// action thunk
+export const getAllDogs = () => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get(
+        // 'https://shelter-in-pets-server.herokuapp.com/api/dogs'
+        'http://localhost:3000/api/dogs'
+      );
+      dispatch(gotAllDogs(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+// Reducer
+const dogs = (state = initalState, action) => {
+  switch (action.type) {
+    case GET_ALL_DOGS:
+      return { ...state, allDogs: action.allDogs };
+    default:
+      return state;
+  }
+};
+
+export default dogs;
