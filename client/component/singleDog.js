4690ec356970685c41cb8e2e46756a9b73801ab7
Angela Vuong <vuong.angela@gmail.com>
2020-03-24 22:51:33 -0400

    fixed: photo for single dog

--- client/component/singleDog.js
+++ client/component/singleDog.js
@@ -6,21 +6,19 @@ function SingleDog({ route }) {
   console.log(route.params.name);
 
   const name = route.params.name;
-  const imageUrl = route.params.photos[0].full;
+  let imageUrl =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS07bU3_0rUVJ9_zj5L78h_3D1aKr4gq1RkZ9YhjEmLLvieGERn';
+  if (route.params.photos[0] !== undefined) {
+    imageUrl = route.params.photos[0].full;
+  }
   const breed = route.params.breeds.primary;
   const description = route.params.description;
   const location = route.params.url;
-  const defaultImageUrl =
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS07bU3_0rUVJ9_zj5L78h_3D1aKr4gq1RkZ9YhjEmLLvieGERn';
 
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 34 }}>{name}</Text>
-      {imageUrl ? (
-        <Image source={{ uri: imageUrl }} style={styles.image} />
-      ) : (
-        <Image source={{ uri: defaultImageUrl }} style={styles.image} />
-      )}
+      <Image source={{ uri: imageUrl }} style={styles.image} />
       <Text>Like button here as well?</Text>
       <Text>{breed}</Text>
       <Text>{description}</Text>
