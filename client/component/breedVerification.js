6d98956a8d82f7ef0e0a2e78126307213129a043
Angela Vuong <vuong.angela@gmail.com>
2020-03-25 17:21:25 -0400

    feat: started breed verification component

---
+++ client/component/breedVerification.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { View, Text, Image, Button, StyleSheet } from 'react-native';
+
+export default class BreedVerification extends Component {
+  constructor() {}
+}
