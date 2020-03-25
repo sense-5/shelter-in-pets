fa8fb900e5752c35c70a357dc634f399bada6f45
Katie Durand <khd25@cornell.edu>
2020-03-25 14:34:57 -0500

    kate changed the image picker view for user in case they want to select a different image than the one they originally clicked - started to make a button that will throw their final decision of image to the teachable machine model

--- ImagePicker.js
+++ ImagePicker.js
@@ -1,68 +1,81 @@
-import React,  {Component} from 'react'
-import {View, Text, Image, Button, StyleSheet} from 'react-native'
-import * as ImagePicker from 'expo-image-picker';
-import {connect} from 'react-redux';
-import {getPickedImage} from './client/store/imagePicker'
+import React, { Component } from "react";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { connect } from "react-redux";
+import { getPickedImage } from "./client/store/imagePicker";
 
 class ImagePick extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     // this.state = {
     //   pickedUri: ''
     // }
-    this.openImagePicker = this.openImagePicker.bind(this)
+    this.openImagePicker = this.openImagePicker.bind(this);
   }
 
-  async openImagePicker(){
-    let pickerResult = await ImagePicker.launchImageLibraryAsync()
+  async openImagePicker() {
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
 
     // this.setState({
     //   pickedUri: pickerResult.uri
     // })
-    this.props.getPickedImage(pickerResult)
+    this.props.getPickedImage(pickerResult);
   }
 
-  render(){
-    console.log('picked image', this.props.pickedImage.pickedImage)
+  render() {
+    console.log("picked image", this.props.pickedImage.pickedImage);
 
-    return(
+    return (
       <View style={styles.container}>
-        {
-        this.props.pickedImage.pickedImage ? (
+        {this.props.pickedImage.pickedImage ? (
           <View>
-          <Text>chosen dog</Text>
-          <Image style={styles.image} source={{uri: this.props.pickedImage.pickedImage}}/>
+            <Text style={styles.text}>See more pups like this one?</Text>
+            <Button
+              title="Yes Please!"
+              onPress={this.SEND_IMAGE_TO_TEACHABLE_MACHINE_MODEL}
+            />
+            <Image
+              style={styles.image}
+              source={{ uri: this.props.pickedImage.pickedImage }}
+            />
+            <Button
+              title="Wait, I'll pick a different one!"
+              onPress={this.openImagePicker}
+            />
           </View>
         ) : (
-          <Text>choose a photo</Text>
-        )
-        }
-
-        <Button title="Choose photo" onPress={this.openImagePicker}/>
+          <View>
+            <Text style={styles.text}>Show us your dream dog 🐾</Text>
+            <Button title="Choose photo" onPress={this.openImagePicker} />
+          </View>
+        )}
       </View>
-    )
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   image: {
     height: 250,
     width: 250
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 18
   }
-})
+});
 
 const mapState = state => ({
   pickedImage: state.pickedImage
-})
+});
 
 const mapDispatch = dispatch => ({
-  getPickedImage: (img) => dispatch(getPickedImage(img.uri))
-})
+  getPickedImage: img => dispatch(getPickedImage(img.uri))
+});
 
-export default connect(mapState,mapDispatch)(ImagePick)
+export default connect(mapState, mapDispatch)(ImagePick);
