5038d03df2b5d59a50dc8c638911b878aa4079fe
Angela Vuong <vuong.angela@gmail.com>
2020-03-26 09:21:31 -0400

    workng on breed verification

--- ImagePicker.js
+++ ImagePicker.js
@@ -1,56 +1,55 @@
-import React,  {Component} from 'react'
-import {View, Text, Image, Button, StyleSheet} from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, Image, Button, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 class ImagePick extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      pickedUri: ''
-    }
-    this.openImagePicker = this.openImagePicker.bind(this)
+      pickedUri: '',
+    };
+    this.openImagePicker = this.openImagePicker.bind(this);
   }
 
-  async openImagePicker(){
-    let pickerResult = await ImagePicker.launchImageLibraryAsync()
+  async openImagePicker() {
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
 
     this.setState({
-      pickedUri: pickerResult.uri
-    })
+      pickedUri: pickerResult.uri,
+    });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.container}>
-        {
-        this.state.pickedUri ? (
+        {this.state.pickedUri ? (
           <View>
-          <Text>chosen dog</Text>
-          <Image style={styles.image} source={{uri: this.state.pickedUri}}/>
+            <Text>chosen dog</Text>
+            <Image
+              style={styles.image}
+              source={{ uri: this.state.pickedUri }}
+            />
           </View>
         ) : (
           <Text>choose a photo</Text>
-        )
-        }
+        )}
 
-        <Button title="Choose photo" onPress={this.openImagePicker}/>
+        <Button title="Choose photo" onPress={this.openImagePicker} />
       </View>
-    )
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   image: {
     height: 250,
-    width: 250
-  }
-})
+    width: 250,
+  },
+});
 
-
-export default ImagePick
+export default ImagePick;
--- client/component/breedVerification.js
+++ client/component/breedVerification.js
@@ -2,5 +2,46 @@ import React, { Component } from 'react';
 import { View, Text, Image, Button, StyleSheet } from 'react-native';
 
 export default class BreedVerification extends Component {
-  constructor() {}
+  constructor() {
+    super();
+    this.state = {
+      outputBreed: undefined,
+    };
+
+    this.handleBreedConfirmation = this.handleBreedConfirmation.bind(this);
+  }
+
+  handleBreedConfirmation() {}
+
+  render() {
+    return (
+      <View style={styles.contatiner}>
+        {this.state.outputBreed ? (
+          <View>
+            <Text>Is this the breed you are looking for?</Text>
+            {/* <Image> Maybe static image that matches this.state.outputBreed</Image> */}
+            <Text>{this.state.outputBreed}</Text>
+            <Button title="YES" onPress={this.handleBreedConfirmation} />
+          </View>
+        ) : (
+          <View>
+            <Text>Let us help you find your perfect dog.</Text>
+            <Button title="Set Preferences" />
+          </View>
+        )}
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    height: 250,
+    width: 250,
+  },
+});
