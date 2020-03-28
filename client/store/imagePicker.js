16456d88a61dbdd6d3a8b52c5a6d53536b528cd8
Sonia Susanto <soniasusanto@gmail.com>
2020-03-28 14:41:56 -0400

    working clarifai model

--- client/store/imagePicker.js
+++ client/store/imagePicker.js
@@ -1,29 +1,53 @@
+import Axios from 'axios';
 
 //initial state
 const initialState = {
-  pickedImage: ''
-}
+  pickedImage: '',
+  key: '',
+};
 
 //action type
-const GET_PICKED_IMAGE = 'GET_PICKED_IMAGE'
+const GET_PICKED_IMAGE = 'GET_PICKED_IMAGE';
+const GET_KEY = 'GET_KEY';
 
 //action creator
-export const getPickedImage = (pickedImage) => ({
+export const getPickedImage = pickedImage => ({
   type: GET_PICKED_IMAGE,
-  pickedImage
-})
+  pickedImage,
+});
+
+export const getKey = key => ({
+  type: GET_KEY,
+  key,
+});
+
+//get prediction thunk
+
+export const getClarifaiKey = () => async dispatch => {
+  try {
+    const { data } = await Axios.get('http://localhost:3000/auth/clarifai');
+    dispatch(getKey(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //reducer
 const pickedImage = (state = initialState, action) => {
-  switch (action.type){
+  switch (action.type) {
     case GET_PICKED_IMAGE:
       return {
         ...state,
-        pickedImage: action.pickedImage
-      }
+        pickedImage: action.pickedImage,
+      };
+    case GET_KEY:
+      return {
+        ...state,
+        key: action.key,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
 export default pickedImage;
