de526ec95c04cab1ade76a36d6cc2a67dfd954df
Laura Armfield-Perez <lca13@pitt.edu>
2020-04-01 12:43:32 -0400

    refactor: remove console logs and unused code

--- client/component/searchResults.js
+++ client/component/searchResults.js
@@ -1,29 +1,20 @@
 import React, {Component} from 'react'
 import {
   View,
-  Text,
   StyleSheet,
   Image,
   FlatList,
-  ActivityIndicator,
-  Dimensions,
   TouchableOpacity
 } from 'react-native';
 import {connect} from 'react-redux'
-import {gotRequestedDogs} from '../store/requestedAttributes'
-import AllDogs from './allDogs'
 const defaultDog = require('../../assets/images/dog1.png')
-// import { fetchRequestedDogs } from '../store/requestedAttributes';
 
 class SearchResults extends Component {
   render(){
-    console.log('searCH RESULTS RENDER')
     const dogs = this.props.reqDogs
 
     return(
       <View>
-         {/* <AllDogs navigation={this.props.navigation} allDogs={dogs}/> */}
-        {/* <Text>search results</Text> */}
         <FlatList
             numColumns={2}
             data={dogs}
@@ -38,7 +29,6 @@ class SearchResults extends Component {
                   onPress={() => {
                     this.props.navigation.navigate('Single Dog', item);
                   }}
-                  // this.view(item.dog);
                 >
                 {item.photos.length ?  (
                   <Image source={{ uri: item.photos[0].full }} style={styles.image} />
@@ -75,9 +65,4 @@ const mapState = state => ({
   reqDogs: state.requestedAttributes.requestedDogs
 })
 
-// const mapDispatch = dispatch => ({
-//   fetchRequestedDogs: (req) => dispatch(fetchRequestedDogs(req))
-// })
-
 export default connect(mapState, null)(SearchResults)
-// export default SearchResults
