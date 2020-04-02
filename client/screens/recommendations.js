fd65988efe60d24c719f8aee4ee2e811c4ebe38e
Katie Durand <khd25@cornell.edu>
2020-04-02 15:36:11 -0500

    fixed recs and added styling

--- client/screens/recommendations.js
+++ client/screens/recommendations.js
@@ -13,22 +13,6 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { connect } from 'react-redux';
 import { getRecDogs } from '../store/recommendations';
 
-// const dogImg = [
-//   {
-//     uri:
-//       'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*'
-//   },
-//   {
-//     uri:
-//       'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-dog-quotes-1580508958.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*'
-//   },
-//   { uri: 'https://content.fortune.com/wp-content/uploads/2019/01/boo.jpg' },
-//   {
-//     uri:
-//       'https://www.washingtonpost.com/resizer/uwlkeOwC_3JqSUXeH8ZP81cHx3I=/arc-anglerfish-washpost-prod-washpost/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg'
-//   }
-// ];
-
 export class Recommendations extends Component {
   constructor() {
     super();
@@ -40,41 +24,53 @@ export class Recommendations extends Component {
 
   render() {
     let dogs;
-    if (this.props.defaultDogs) {
+    if (this.props.defaultDogs.length !== 0) {
       dogs = this.props.defaultDogs;
     } else {
       dogs = this.props.recDogs;
     }
     return (
-      <ScrollView scrollEventThrottle={16}>
-        <View style={styles.container}>
-          <Text style={styles.text}>Recommended based on your activity</Text>
-          <View style={styles.imgContainer}>
-            <ScrollView
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-            >
-              {dogs.map((dog, idx) => {
-                return (
-                  <View key={idx}>
-                    <View>
-                      <TouchableOpacity>
-                        <Image
-                          style={styles.img}
-                          source={{
-                            uri: dog.uri
-                          }}
-                        />
-                        <Text style={styles.text}>{dog.dog.name}</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                );
-              })}
-            </ScrollView>
-          </View>
+      <View style={styles.outerContainer}>
+        <View>
+          <Text style={styles.topHeader}>Recommendations</Text>
         </View>
-      </ScrollView>
+        <ScrollView scrollEventThrottle={16}>
+          <View style={styles.container}>
+            <Text style={styles.text}>based on your recent activity 🐾</Text>
+            <View style={styles.imgContainer}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+              >
+                {dogs.map((dog, idx) => {
+                  return (
+                    <View key={idx}>
+                      <View>
+                        <TouchableOpacity
+                          onPress={() =>
+                            this.props.navigation.navigate(
+                              'Single Dog',
+                              dog.dog
+                            )
+                          }
+                        >
+                          <Image
+                            style={styles.img}
+                            source={{
+                              uri: dog.uri
+                            }}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  );
+                })}
+                {/* <Bullets /> set up bullets */}
+              </ScrollView>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     );
   }
 }
@@ -98,22 +94,39 @@ const Recs = connect(mapStateToProps, mapDispatchToProps)(Recommendations);
 export default Recs;
 
 const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: 20
+    paddingTop: '7%'
   },
   text: {
-    fontSize: 24,
-    fontWeight: '700',
-    paddingHorizontal: 20
+    textAlign: 'center',
+    fontSize: 22,
+    padding: 5,
+    marginBottom: 20,
+    color: '#147efb',
+    fontWeight: 'bold'
   },
   imgContainer: {
     height: '100%',
     marginTop: 20
   },
   img: {
-    height: 300,
+    height: 450,
     width: 450
+  },
+  topHeader: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: 1000,
+    color: '#147efb',
+    backgroundColor: 'white',
+    padding: 10
   }
 });
